2e9e2152f00a810a492bee0970dcf9ef809ad42c
prabhjeet13 <prabhjeetsingh2000@gmail.com>
2025-01-12 14:27:07 +0530

    url changes with render url

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,46 +1,93 @@
 export const doctor = {
-    signup : 'http://localhost:4001/api/v1/doctor/signupdoctor',
-    signin : 'http://localhost:4001/api/v1/doctor/signindoctor',
-    getdoctorsbyspecialities : 'http://localhost:4001/api/v1/doctor/getdoctorsbyspeciality',
-    getdoctorsbyid : 'http://localhost:4001/api/v1/doctor/getdoctorbyid',
-    editdetails : 'http://localhost:4001/api/v1/doctor/editdoctordetails',
-    getalldoctorsactive : 'http://localhost:4001/api/v1/doctor/getalldoctors',
-    getalldoctorspending : 'http://localhost:4001/api/v1/doctor/getalldoctorspending',
-    editdoctorAvailaility : 'http://localhost:4001/api/v1/doctor/editdoctoravailability',
-    deleteDoctoravAilability : 'http://localhost:4001/api/v1/doctor/deletedoctoravailability',
-    done_appointment : 'http://localhost:4001/api/v1/doctor/done_appointment',
-    my_appointments : 'http://localhost:4001/api/v1/doctor/my_appointments'
+    signup : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/signupdoctor',
+    signin : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/signindoctor',
+    getdoctorsbyspecialities : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/getdoctorsbyspeciality',
+    getdoctorsbyid : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/getdoctorbyid',
+    editdetails : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/editdoctordetails',
+    getalldoctorsactive : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/getalldoctors',
+    getalldoctorspending : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/getalldoctorspending',
+    editdoctorAvailaility : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/editdoctoravailability',
+    deleteDoctoravAilability : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/deletedoctoravailability',
+    done_appointment : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/done_appointment',
+    my_appointments : 'https://medisphere-ydwu.onrender.com/api/v1/doctor/my_appointments'
 }
 
 export const patient = {
-    signup : 'http://localhost:4001/api/v1/patient/signuppatient',
-    signin : 'http://localhost:4001/api/v1/patient/signinpatient',
-    getpatientbyid : 'http://localhost:4001/api/v1/patient/getpatientbyid',
-    editdetails : 'http://localhost:4001/api/v1/patient/editpatientdetails',
-    appointment : 'http://localhost:4001/api/v1/patient/appointment_capturePayment',
-    SEND_PAYMENT_EMAIL : 'http://localhost:4001/api/v1/patient/sendpaymentsuccess',
-    VERIFY_PAYMENT_API : 'http://localhost:4001/api/v1/patient/verifysignature',
-    my_appointments : 'http://localhost:4001/api/v1/patient/my_appointments'
+    signup : 'https://medisphere-ydwu.onrender.com/api/v1/patient/signuppatient',
+    signin : 'https://medisphere-ydwu.onrender.com/api/v1/patient/signinpatient',
+    getpatientbyid : 'https://medisphere-ydwu.onrender.com/api/v1/patient/getpatientbyid',
+    editdetails : 'https://medisphere-ydwu.onrender.com/api/v1/patient/editpatientdetails',
+    appointment : 'https://medisphere-ydwu.onrender.com/api/v1/patient/appointment_capturePayment',
+    SEND_PAYMENT_EMAIL : 'https://medisphere-ydwu.onrender.com/api/v1/patient/sendpaymentsuccess',
+    VERIFY_PAYMENT_API : 'https://medisphere-ydwu.onrender.com/api/v1/patient/verifysignature',
+    my_appointments : 'https://medisphere-ydwu.onrender.com/api/v1/patient/my_appointments'
 }
 
 export const admin = {
-    signup : 'http://localhost:4001/api/v1/admin/signupadmin',
-    signin : 'http://localhost:4001/api/v1/admin/signinadmin',
-    givepermission : 'http://localhost:4001/api/v1/admin/givepermission',
-    editdetails : 'http://localhost:4001/api/v1/admin/editadmindetails'
+    signup : 'https://medisphere-ydwu.onrender.com/api/v1/admin/signupadmin',
+    signin : 'https://medisphere-ydwu.onrender.com/api/v1/admin/signinadmin',
+    givepermission : 'https://medisphere-ydwu.onrender.com/api/v1/admin/givepermission',
+    editdetails : 'https://medisphere-ydwu.onrender.com/api/v1/admin/editadmindetails'
 }
 
 export const specialization =  {
-    add : 'http://localhost:4001/api/v1/specialization/addspecialiation',
-    getall : 'http://localhost:4001/api/v1/specialization/getallspecializations'
+    add : 'https://medisphere-ydwu.onrender.com/api/v1/specialization/addspecialiation',
+    getall : 'https://medisphere-ydwu.onrender.com/api/v1/specialization/getallspecializations'
 }
 
 export const otp = {
-    sendotp : 'http://localhost:4001/api/v1/otp/sendotp',
+    sendotp : 'https://medisphere-ydwu.onrender.com/api/v1/otp/sendotp',
 }
 
 export const conversation = {
-    getMyconversation : 'http://localhost:4001/api/v1/messages/getmyconversation',
-    getMessage : 'http://localhost:4001/api/v1/messages/getmessages',
-    sendMessage : 'http://localhost:4001/api/v1/messages/sendmessage',
+    getMyconversation : 'https://medisphere-ydwu.onrender.com/api/v1/messages/getmyconversation',
+    getMessage : 'https://medisphere-ydwu.onrender.com/api/v1/messages/getmessages',
+    sendMessage : 'https://medisphere-ydwu.onrender.com/api/v1/messages/sendmessage',
 }
+
+// export const doctor = {
+//     signup : 'http://localhost:4001/api/v1/doctor/signupdoctor',
+//     signin : 'http://localhost:4001/api/v1/doctor/signindoctor',
+//     getdoctorsbyspecialities : 'http://localhost:4001/api/v1/doctor/getdoctorsbyspeciality',
+//     getdoctorsbyid : 'http://localhost:4001/api/v1/doctor/getdoctorbyid',
+//     editdetails : 'http://localhost:4001/api/v1/doctor/editdoctordetails',
+//     getalldoctorsactive : 'http://localhost:4001/api/v1/doctor/getalldoctors',
+//     getalldoctorspending : 'http://localhost:4001/api/v1/doctor/getalldoctorspending',
+//     editdoctorAvailaility : 'http://localhost:4001/api/v1/doctor/editdoctoravailability',
+//     deleteDoctoravAilability : 'http://localhost:4001/api/v1/doctor/deletedoctoravailability',
+//     done_appointment : 'http://localhost:4001/api/v1/doctor/done_appointment',
+//     my_appointments : 'http://localhost:4001/api/v1/doctor/my_appointments'
+// }
+
+// export const patient = {
+//     signup : 'http://localhost:4001/api/v1/patient/signuppatient',
+//     signin : 'http://localhost:4001/api/v1/patient/signinpatient',
+//     getpatientbyid : 'http://localhost:4001/api/v1/patient/getpatientbyid',
+//     editdetails : 'http://localhost:4001/api/v1/patient/editpatientdetails',
+//     appointment : 'http://localhost:4001/api/v1/patient/appointment_capturePayment',
+//     SEND_PAYMENT_EMAIL : 'http://localhost:4001/api/v1/patient/sendpaymentsuccess',
+//     VERIFY_PAYMENT_API : 'http://localhost:4001/api/v1/patient/verifysignature',
+//     my_appointments : 'http://localhost:4001/api/v1/patient/my_appointments'
+// }
+
+// export const admin = {
+//     signup : 'http://localhost:4001/api/v1/admin/signupadmin',
+//     signin : 'http://localhost:4001/api/v1/admin/signinadmin',
+//     givepermission : 'http://localhost:4001/api/v1/admin/givepermission',
+//     editdetails : 'http://localhost:4001/api/v1/admin/editadmindetails'
+// }
+
+// export const specialization =  {
+//     add : 'http://localhost:4001/api/v1/specialization/addspecialiation',
+//     getall : 'http://localhost:4001/api/v1/specialization/getallspecializations'
+// }
+
+// export const otp = {
+//     sendotp : 'http://localhost:4001/api/v1/otp/sendotp',
+// }
+
+// export const conversation = {
+//     getMyconversation : 'http://localhost:4001/api/v1/messages/getmyconversation',
+//     getMessage : 'http://localhost:4001/api/v1/messages/getmessages',
+//     sendMessage : 'http://localhost:4001/api/v1/messages/sendmessage',
+// }
